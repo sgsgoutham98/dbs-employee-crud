539e3d611f4690194dfc06a2633df80c85d4b0a1
pradeep.kumar44@gmail.com <pradeep.kumar44@gmail.com>
2019-08-05 23:07:07 +0530

    committing the hibernate code

---
+++ src/main/java/com/dbs/emp_mgmt/dao/HibernateDAOImpl.java
@@ -0,0 +1,44 @@
+package com.dbs.emp_mgmt.dao;
+
+import com.dbs.emp_mgmt.model.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ejb.access.LocalStatelessSessionProxyFactoryBean;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("hibernateDAO")
+public class HibernateDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Employee save(Employee employee) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.saveOrUpdate(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee update(long empId, Employee employee) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    @Override
+    public Employee findById(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(long id) {
+
+    }
+}
