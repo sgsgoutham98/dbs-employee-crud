9b7ae7730164bf6113b2627e40b47fb38ca41140
pradeep.kumar44@gmail.com <pradeep.kumar44@gmail.com>
2019-08-05 11:53:17 +0530

    completed CRUD implementation

--- src/main/java/com/dbs/emp_mgmt/client/EmpManangementClient.java
+++ src/main/java/com/dbs/emp_mgmt/client/EmpManangementClient.java
@@ -8,12 +8,33 @@ import java.time.LocalDate;
 
 public class EmpManangementClient {
 
+    private static  EmployeeController employeeController;
+
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
-        EmployeeController employeeController = applicationContext.getBean(EmployeeController.class);
+        employeeController = applicationContext.getBean(EmployeeController.class);
+        saveEmployees();
+        listAllEmployees();
+        fetchEmployeeDetailsById(12);
+
+    }
+
+    private static void fetchEmployeeDetailsById(int i) {
+        System.out.println("Employee with id "+i +" is " +employeeController.findById(i));
+    }
+
+    private static void listAllEmployees() {
+        employeeController.listAll().forEach(employee -> System.out.println(employee));
+    }
+
+
+
+    private static void saveEmployees(){
         Employee employee = new Employee(12,"Harish", LocalDate.of(1985,5, 25));
-
         employeeController.saveEmployee(employee);
-
+        employee = new Employee(15,"VInayak", LocalDate.of(1985,6, 18));
+        employeeController.saveEmployee(employee);
+        employee = new Employee(12,"Harish", LocalDate.of(1981,3, 25));
+        employeeController.saveEmployee(employee);
     }
 }
